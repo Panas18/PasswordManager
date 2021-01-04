25561f21ecf6ad292167e864baef6107c9ff82f1
Panas18 <panas.tiwary18@gmail.com>
2021-01-04 10:22:46 +0545

    Added error message when no argument is passed

--- pasman.py
+++ pasman.py
@@ -44,7 +44,17 @@ class User:
         except KeyboardInterrupt:
             print(f"{bcolors.FAIL}User Interrupt!")
             sys.exit()
-
+try:
+    U = User()
+    U.data.user_data_base = pd.read_csv(file_name)
+except FileNotFoundError:
+    print(f"{bcolors.OKBLUE}Database not created!!!{bcolors.ENDC}")
+    print(f"{bcolors.OKBLUE}Enter a MasterPassword to create database{bcolors.ENDC}")
+    password = sc.Get_Master_Password()
+    U.data.Create_User_Database()
+    key = En.Generate_Key(password)
+    En.encrypt(key, file_name)
+    sys.exit()
 
 parser = argparse.ArgumentParser(description="CLI Password Manager")
 
@@ -62,99 +72,92 @@ parser.add_argument("-R", "--remove_all", action='store_true',
 
 args = parser.parse_args()
 
-try:
-    U = User()
-    U.data.user_data_base = pd.read_csv(file_name)
-except FileNotFoundError:
-    print(f"{bcolors.OKBLUE}Database not created!!!{bcolors.ENDC}")
-    print(f"{bcolors.OKBLUE}Enter a MasterPassword to create database{bcolors.ENDC}")
-    password = sc.Get_Master_Password()
-    U.data.Create_User_Database()
-    key = En.Generate_Key(password)
-    En.encrypt(key, file_name)
-    sys.exit()
 
-try:
-    if args.add_password:
-        try:
+if len(sys.argv) > 1:
+    try:
+        if args.add_password:
+            try:
+                U = User()
+                pass_value = U.Identify_User()
+                if pass_value:
+                    U.data.app = input(
+                        f"{bcolors.OKGREEN}App:{bcolors.ENDC} ")
+                    U.data.login_id = input(
+                        f"{bcolors.OKGREEN}Login Id:{bcolors.ENDC} ")
+                    partial_data_base = U.data.user_data_base[U.data.user_data_base["App"] == U.data.app]
+                    login_id = list(partial_data_base["LoginId"])
+                    try:
+                        if U.data.login_id in login_id:
+                            raise ValueError
+                        check = str(
+                            input(f"{bcolors.OKGREEN}Generate Password? [Yes/No:]{bcolors.ENDC} ")).lower()
+                        if check[0] == "y":
+                            try:
+                                pass_length = int(
+                                    input(f"{bcolors.OKGREEN}Password Length:{bcolors.ENDC} "))
+                                U.data.password = sc.Generate_Password(
+                                    len=pass_length)
+                            except Exception as err:
+                                print(
+                                    f"{bcolors.WARNING}password Length is 8 when defult")
+                                U.data.password = sc.Generate_Password()
+                        else:
+                            U.data.password = input(
+                                f"{bcolors.OKGREEN}Password:{bcolors.ENDC} ")
+                        U.data.Add_Password()
+                    except ValueError:
+                        print("App and LoginId already exists")
+                        En.encrypt(key, file_name)
+                        sys.exit()
+            except Exception as err:
+                print(f"{bcolors.FAIL}{err}")
+                # print(err)
+
+                sys.exit()
+
+        if args.view_password:
             U = User()
             pass_value = U.Identify_User()
             if pass_value:
-                U.data.app = input(
-                    f"{bcolors.OKGREEN}App:{bcolors.ENDC} ")
-                U.data.login_id = input(
-                    f"{bcolors.OKGREEN}Login Id:{bcolors.ENDC} ")
-                partial_data_base = U.data.user_data_base[U.data.user_data_base["App"] == U.data.app]
-                login_id = list(partial_data_base["LoginId"])
+                U.data.app = str(
+                    input(f"{bcolors.OKGREEN}App:{bcolors.ENDC} ")).lower().split(".")[0]
+                login_id, password = U.data.Return_Password()
+                print(f"Login Id:{bcolors.WARNING} {login_id}")
                 try:
-                    if U.data.login_id in login_id:
-                        raise ValueError
+                    pyperclip.copy(password)
+                    print(f"{bcolors.OKBLUE}Password is copied to your clipboard")
                     check = str(
-                        input(f"{bcolors.OKGREEN}Generate Password? [Yes/No:]{bcolors.ENDC} ")).lower()
+                        input((f"{bcolors.OKGREEN}View Password? [Yes/No]:{bcolors.ENDC} "))).lower()
                     if check[0] == "y":
-                        try:
-                            pass_length = int(
-                                input(f"{bcolors.OKGREEN}Password Length:{bcolors.ENDC} "))
-                            U.data.password = sc.Generate_Password(
-                                len=pass_length)
-                        except Exception as err:
-                            print(
-                                f"{bcolors.WARNING}password Length is 8 when defult")
-                            U.data.password = sc.Generate_Password()
+                        print(f"Password:{bcolors.WARNING} {password}")
                     else:
-                        U.data.password = input(
-                            f"{bcolors.OKGREEN}Password:{bcolors.ENDC} ")
-                    U.data.Add_Password()
-                except ValueError:
-                    print("App and LoginId already exists")
-                    En.encrypt(key, file_name)
+                        sys.exit()
+                except Exception as err:
+                    print(f"{bcolors.WARNING}No when default")
                     sys.exit()
-        except Exception as err:
-            print(f"{bcolors.FAIL}{err}")
-            # print(err)
 
-            sys.exit()
+        if args.list_passwords:
+            U = User()
+            pass_value = U.Identify_User()
+            if pass_value:
+                U.data.List_Passwords()
 
-    if args.view_password:
-        U = User()
-        pass_value = U.Identify_User()
-        if pass_value:
-            U.data.app = str(
-                input(f"{bcolors.OKGREEN}App:{bcolors.ENDC} ")).lower().split(".")[0]
-            login_id, password = U.data.Return_Password()
-            print(f"Login Id:{bcolors.WARNING} {login_id}")
-            try:
-                pyperclip.copy(password)
-                print(f"{bcolors.OKBLUE}Password is copied to your clipboard")
-                check = str(
-                    input((f"{bcolors.OKGREEN}View Password? [Yes/No]:{bcolors.ENDC} "))).lower()
-                if check[0] == "y":
-                    print(f"Password:{bcolors.WARNING} {password}")
-                else:
-                    sys.exit()
-            except Exception as err:
-                print(f"{bcolors.WARNING}No when default")
-                sys.exit()
+        if args.remove_pass:
+            U = User()
+            pass_value = U.Identify_User()
+            if pass_value:
+                U.data.Remove_Password()
 
-    if args.list_passwords:
-        U = User()
-        pass_value = U.Identify_User()
-        if pass_value:
-            U.data.List_Passwords()
+        if args.remove_all:
+            U = User()
+            pass_value = U.Identify_User()
+            if pass_value:
+                U.data.Remove_User()
 
-    if args.remove_pass:
-        U = User()
-        pass_value = U.Identify_User()
-        if pass_value:
-            U.data.Remove_Password()
-
-    if args.remove_all:
-        U = User()
-        pass_value = U.Identify_User()
-        if pass_value:
-            U.data.Remove_User()
-
-except KeyboardInterrupt:
-    print(f"{bcolors.FAIL}User Interrupt")
-    En.encrypt(key, file_name)
-    sys.exit()
+    except KeyboardInterrupt:
+        print(f"{bcolors.FAIL}User Interrupt")
+        En.encrypt(key, file_name)
+        sys.exit()
+else:
+    print(f"{bcolors.FAIL}Please Enter an argument")
+    print(f"Run -h for help{bcolors.ENDC}")
